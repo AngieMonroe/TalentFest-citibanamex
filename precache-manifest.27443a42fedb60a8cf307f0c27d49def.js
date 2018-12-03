5eeb57d85347c83585bed80451dc9520ed8c6d08
AngieMonroe <ame.monroy@gmail.com>
2018-12-03 17:44:05 -0600

    Updates

---
+++ precache-manifest.27443a42fedb60a8cf307f0c27d49def.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "873d43ea03014c13b9fd",
+    "url": "/static/js/main.873d43ea.chunk.js"
+  },
+  {
+    "revision": "daba63f6a98d079ad0ce",
+    "url": "/static/js/1.daba63f6.chunk.js"
+  },
+  {
+    "revision": "873d43ea03014c13b9fd",
+    "url": "/static/css/main.2c80afc0.chunk.css"
+  },
+  {
+    "revision": "b9e22eb19a59ffe61fc40322ea361e5f",
+    "url": "/index.html"
+  }
+];
